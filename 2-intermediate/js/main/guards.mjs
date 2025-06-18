8a888ca260279265e409ab05b6690ef82a5fa84e
Gepoverlow <gepeoverload@gmail.com>
2025-06-18 11:44:02 +0200

    added guards, validators and transformers + unit tests

---
+++ 2-intermediate/js/main/guards.mjs
@@ -0,0 +1,41 @@
+// Write a function that checks that the value type is number.
+export function isNumber(value) {
+
+    // Your code
+
+};
+
+// Write a function that checks that the value type is string.
+export function isString(value) {
+
+    // Your code
+
+};
+
+// Write a function that checks that the value type is array.
+export function isArray(value) {
+
+    // Your code
+
+};
+
+// Write a function that checks is value is a plain object (not null, not array).
+export function isPlainObject(value) {
+
+    // Your code
+
+};
+
+// Write a function that checks if value has a specific property (optionally or a specific type).
+export function hasProperty(value, key) {
+
+    // Your code
+
+};
+
+// Write a function that checks if a value has a 'name' property.
+export function hasNameProperty(value) {
+
+    // Your code
+
+};
